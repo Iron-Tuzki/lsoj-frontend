178b9373b38d9b2d2479de9cc808b8fdae02be22
Iron Tuzki <759741749@qq.com>
2023-11-21 10:38:32 +0800

    nothing

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,16 @@
 import { RouteRecordRaw } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+import AboutView from "@/views/AboutView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "你好",
+    path: "/home",
+    name: "home",
     component: HelloWorld,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView
   },
 ];
